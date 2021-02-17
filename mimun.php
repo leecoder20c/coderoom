7077e181b9f4de76955007ab1b31e7b3ed0be994
Lee SeungJu <061@mediabrain.co.kr>
2021-02-17 10:06:50 +0900

    [weather] weather.php 삭제, ajax용 소스 크로스사이트이슈 테스트

--- mimun.php
+++ mimun.php
@@ -1,18 +1,24 @@
 <?php
-    $ch = curl_init();//curl 초기화
-    curl_setopt($ch, CURLOPT_URL, 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst?serviceKey=bqe2eZbyRVgn4WUOJVUwtct8lxVxy%2B%2BaDAW3bywvmgTHVKaztYZz5qBJQcQOe%2BQVj%2FuuvbPCMHDh6Yz5ZFYlRw%3D%3D&numOfRows=1&pageNo=1&itemCode=PM10&dataGubun=HOUR&searchCondition=MONTH');//URL 지정
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);//요청결과 문자열로 반환
-    curl_setopt($ch, CURLOPT_TIMEOUT, 10);//요청 타임아웃
-    //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);//원격 서버의 인증서가 유효한지 검사 안함
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, $data);//POST data
-    //curl_setopt($ch, CURLOPT_POST, true);//true시 post 전송
+  header('Access-Control-Allow-Origin: https://leecoder20c.github.io, http://106.248.232.170, http://192.168.11.34:9898');
+  header('Access-Control-Max-Age: 86400');
+  header('Access-Control-Allow-Headers: Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization');
+  header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+  header("Content-type:text/html;charset=utf-8");
+  
+  $ch = curl_init();//curl 초기화
+  curl_setopt($ch, CURLOPT_URL, 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst?serviceKey=bqe2eZbyRVgn4WUOJVUwtct8lxVxy%2B%2BaDAW3bywvmgTHVKaztYZz5qBJQcQOe%2BQVj%2FuuvbPCMHDh6Yz5ZFYlRw%3D%3D&numOfRows=1&pageNo=1&itemCode=PM10&dataGubun=HOUR&searchCondition=MONTH');//URL 지정
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);//요청결과 문자열로 반환
+  curl_setopt($ch, CURLOPT_TIMEOUT, 10);//요청 타임아웃
+  //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);//원격 서버의 인증서가 유효한지 검사 안함
+  //curl_setopt($ch, CURLOPT_POSTFIELDS, $data);//POST data
+  //curl_setopt($ch, CURLOPT_POST, true);//true시 post 전송
 
-    $response = curl_exec($ch);
-    $errno = curl_errno($ch);
+  $response = curl_exec($ch);
+  $errno = curl_errno($ch);
 
-    $fileContents = str_replace(array("\n", "\r", "\t"), '', $response);
-    $fileContents = trim(str_replace('"', "'", $fileContents));
-    $simpleXml = simplexml_load_string($fileContents);
-    $json = json_encode($simpleXml);
-    echo $json;
+  $fileContents = str_replace(array("\n", "\r", "\t"), '', $response);
+  $fileContents = trim(str_replace('"', "'", $fileContents));
+  $simpleXml = simplexml_load_string($fileContents);
+  $json = json_encode($simpleXml);
+  echo $json;
 ?>
